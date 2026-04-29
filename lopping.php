87168d40ff4cca6436ad9714de842aed378e051f
maharanisulis101-ops <maharanisulis101@gmail.com>
2026-04-29 20:02:54 +0700

    Add files via upload

--- lopping.php
+++ lopping.php
@@ -5,6 +5,13 @@ $jumlah = 240;
 $setengah = $jumlah / 2;
 for ($i = $jumlah; $i > 0; $i--) {
     echo "tek kotek kotek kotek anak singa turun berkotek, anak singa turunlah $i <br>";
+    
+    $sisa = $i - 1;
+    if ($sisa > 0) {
+        echo "mati satu tinggallah $sisa <br>";
+    } else {
+        echo "mati satu tinggallah induknya <br>";
+    }
 
     if ($i == $setengah){
         echo "induk singa berkata: anak ku kemana? kenapa tinggal setengahnya? main kemana mereka? <br>";
@@ -17,6 +24,13 @@ echo "lagu anak singa. <br>";
 $i = $jumlah;
 while ($i > 0) {
     echo "tek kotek kotek kotek anak singa turun berkotek, anak singa turunlah $i <br>";
+    
+    $sisa = $i - 1;
+    if ($sisa > 0) {
+        echo "mati satu tinggallah $sisa <br>";
+    } else {
+        echo "mati satu tinggallah induknya <br>";
+    }
 
     if ($i == $setengah){
         echo "induk singa berkata: anak ku kemana? kenapa tinggal setengahnya? main kemana mereka? <br>";
@@ -30,6 +44,13 @@ echo "lagu anak singa. <br>";
 $i = $jumlah;
 do {
     echo "tek kotek kotek kotek anak singa turun berkotek, anak singa turunlah $i <br>";
+    
+    $sisa = $i - 1;
+    if ($sisa > 0) {
+        echo "mati satu tinggallah $sisa <br>";
+    } else {
+        echo "mati satu tinggallah induknya <br>";
+    }
 
     if ($i == $setengah){
         echo "induk singa berkata: anak ku kemana? kenapa tinggal setengahnya? main kemana mereka? <br>";
@@ -40,12 +61,16 @@ echo "<hr>";
 
 //looping foreach
 echo "lagu anak singa. <br>";
-$jumlah = 240;
 $data = range($jumlah, 1);
-$setengah = ceil($jumlah / 2);
-
 foreach ($data as $a) {
-    echo "tek kotek kotek kotek anak singa turun berkotek, anak singa turunlah " . $a . "<br>";
+    echo "tek kotek kotek kotek anak singa turun berkotek, anak singa turunlah $a <br>";
+    
+    $sisa = $a - 1;
+    if ($sisa > 0) {
+        echo "mati satu tinggallah $sisa <br>";
+    } else {
+        echo "mati satu tinggallah induknya <br>";
+    }
 
     if ($a == $setengah){
         echo "induk singa berkata: anak ku kemana? kenapa tinggal setengahnya? main kemana mereka? <br>";
